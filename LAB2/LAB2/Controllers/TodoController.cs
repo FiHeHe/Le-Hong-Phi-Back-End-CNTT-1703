da205491117d3431a2afd97f246313c640bd0a6a
FiHeHe <philc0605@gmail.com>
2025-05-21 13:06:58 +0700

    update lab 2

---
+++ LAB2/LAB2/Controllers/TodoController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LAB2.Models;
+
+namespace TodoApp.Controllers
+{
+    public class TodoController : Controller
+    {
+        private static List<Todo> todos = new List<Todo>
+        {
+            new Todo { Id = 1, Task = "Ôn tập HTML" },
+            new Todo { Id = 2, Task = "Ôn tập CSS" },
+            new Todo { Id = 3, Task = "Ôn tập Bootstrap" }
+        };
+
+        public IActionResult Index()
+        {
+            return View(todos);
+        }
+
+        // Action Add
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Todo todo)
+        {
+            todo.Id = todos.Max(t => t.Id) + 1;
+            todos.Add(todo);
+            return RedirectToAction("Index");
+        }
+
+        // Action Edit
+        public IActionResult Edit(int id)
+        {
+            var todo = todos.FirstOrDefault(t => t.Id == id);
+            return View(todo);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Todo updatedTodo)
+        {
+            var todo = todos.FirstOrDefault(t => t.Id == updatedTodo.Id);
+            if (todo != null)
+            {
+                todo.Task = updatedTodo.Task;
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ LAB2/LAB2/Models/Todo.cs
@@ -0,0 +1,6 @@
+﻿namespace LAB2.Models;
+public class Todo
+{
+    public int Id { get; set; }
+    public string Task { get; set; }
+}
